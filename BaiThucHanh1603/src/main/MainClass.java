24a187da230e2b89125a038f73b3e5ce755e19e3
unknown <manhgiang2k2@gmail.com>
2022-03-16 17:07:11 +0700

    Bai 16/03

---
+++ BaiThucHanh1603/src/main/MainClass.java
@@ -0,0 +1,31 @@
+package main;
+
+import shapes.HinhchuNhat;
+import shapes.HinhTron;
+
+public class MainClass {
+
+    public static void main(String[] args){
+        //Khai báo các đối tượng bằng từ khóa new
+        HinhTron hinhTron = new HinhTron();
+        HinhChuNhat hinhChuNhat = new HinhChuNhat();
+
+        //Nhập dữ liệu vào cho hinhTron , tính chu vi , diện tích
+        hinhTron.nhapBanKinh();
+        hinhTron.tinhChuVi();
+        hinhTron.inChuVi();
+        hinhTron.inDienTich();
+
+        //Ngăn cách các hình cho người dùng đỡ nhầm
+        System.out.println("\n\n");
+
+        //Nhập dữ liệu vào cho hinhChuNhat , tính chu vi , diện tích
+        hinhChuNhat.nhapChieuDai();
+        hinhChuNhat.nhapChieuRong();
+        hinhChuNhat.tinhChuVi();
+        hinhChuNhat.tinhDienTich();
+        hinhChuNhat.inChuVi();
+        hinhChuNhat.inDienTich();
+
+    }
+}
